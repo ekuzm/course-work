ddc19ba34a048c0bd5c3f4e4ccd00bf1c40d2f2f
Egor Kuzmenkov <ecuzmenckov@gmail.com>
2025-11-18 18:42:39 +0300

    Fix some bugs

--- include/entities/company.h
+++ include/entities/company.h
@@ -56,7 +56,7 @@ class Company {
     Company(QString name, QString industry, QString location, int foundedYear);
     Company(Company&& other) noexcept;
 
-    ~Company() noexcept = default;
+    ~Company() noexcept;
 
     TaskAssignmentManager& getTaskManager() { return taskManager; }
     const CompanyStatistics& getStatistics() const { return statistics; }
